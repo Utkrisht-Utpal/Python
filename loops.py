d436f1049bfcad2be29c10df9700b4f304196b5b
Utkrisht Utpal <utkrishtutpal1@gmail.com>
2025-12-17 19:18:12 +0530

    Learnt about loops and star printing patterns

---
+++ loops.py
@@ -0,0 +1,44 @@
+# for i in range(1, 101):
+#     print(i)
+
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+# fruits = ["apple", "banana", "lichi", "mango"]
+
+# for i in range (1, len(fruits) + 1):
+#     print(f"{i}: {fruits[i - 1].title()}")
+
+
+# n = int(input("Enter a number upto which you want the sum: "))
+# total = 0
+# for i in range(1, n + 1):
+#     total += i
+# print(total)
+
+
+# # loop for multiplication
+# n = int(input("Enter a number for which you want the table: "))
+# for i in range(1, 11):
+#     print(n * i)
+
+
+# #right angled triangle
+# n = 1
+# while  n <= 5:
+#     print("*" * n)
+#     n += 1
+
+# #right angled skewed triangle
+# for i in range (5, 0, -1):
+#     print("*" * i)
+    
+
+# # Left-aligned triangle
+# n = 6
+# for i in range(1, n):
+#     print(" " * (n - i) + "*" * i)
+
+
