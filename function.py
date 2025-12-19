681d7b03d884c13ca7df053329fc6e9ef4e2615f
Utkrisht Utpal <utkrishtutpal1@gmail.com>
2025-12-19 09:20:58 +0530

    Learnt about functions

---
+++ function.py
@@ -0,0 +1,38 @@
+# def calc(a,b):
+#     print("Sum = ", a + b)
+#     print("Difference = ", a - b)
+
+# calc(2,4)
+# calc(13,8)
+# calc(9,1)
+
+# def wel(name, age):
+#     print(f"Welcome {name} and your age is: {age}")
+
+# wel('Utpal', 18)
+
+# def calc(a = 10, b = 20):
+#     return a*b
+
+# result = calc(20, 20)
+# print(result)
+# print(calc(10,10))
+
+# def cnt(userInput):
+#     vowels= "aeiouAEIOU"
+
+#     countVowel = 0
+#     countConsonant = 0
+
+#     for eachChar in userInput:
+#         if (eachChar.isalpha()):
+#             if(eachChar in vowels):
+#                 countVowel += 1
+#             else: countConsonant += 1
+#     return countVowel, countConsonant
+
+# userInput = input("Enter the your name: ")
+
+# vowel, consonant = cnt(userInput)
+# print(f"No of Vowels: {vowel} and No. of consonant: {consonant}")
+
